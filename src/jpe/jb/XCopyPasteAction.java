7b06fd9ab4b196fed671f5465992e5f04eff93b4
Jeremy Philippe <jeremy.philippe@gmail.com>
2016-11-13 01:50:13 +0100

    Fix dependency on missing DataKeys class in CLion, using CommonDataKeys instead (???).

--- src/jpe/jb/XCopyPasteAction.java
+++ src/jpe/jb/XCopyPasteAction.java
@@ -2,14 +2,14 @@ package jpe.jb;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 
 class XCopyPasteAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent event) {
-        Editor editor = event.getData(DataKeys.EDITOR_EVEN_IF_INACTIVE);
+        Editor editor = event.getData(CommonDataKeys.EDITOR_EVEN_IF_INACTIVE);
         if (editor == null)
             return;
         XCopyListener.pasteClipboard(editor);
